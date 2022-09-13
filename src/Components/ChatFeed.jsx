972961f064b9b4b7953468c4f24ccdcd2f3b2307
khadim Hussain <khadimarman112@gmail.com>
2022-09-14 00:00:58 +0500

    strt

--- src/Components/ChatFeed.jsx
+++ src/Components/ChatFeed.jsx
@@ -10,6 +10,7 @@ const ChatFeed = (props  ) => {
   const renderMessages = () => {
     const keys = object.keys(messages);
     return keys.map((key, index)=>{
+      
   const message = messages [key];
   const lastMessageKey = index=== 0? null:keys[index - 1];
   const isMyMessage = userName === message.sender.username;
